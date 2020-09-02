a09349de327850078cf4e9f08606cadc3efe12f7
Allan5829 <allan.farca@gmail.com>
2020-09-01 21:39:03 -0700

    updates routes with a new route and comments. Creates basic controller actions for sessions and creates 2 basic views for sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController 
+
+    def new
+    end 
+
+    def create
+    end 
+
+    def login
+    end 
+
+    def logging_in
+    end 
+
+    def logout
+    end 
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #For sessions
-  get '/users/signup', to: 'sessions#new', as: :signup 
-  post '/signup', to: 'sessions#create' 
-  get '/users/login', to: 'sessions#login', as: :login 
-  post '/login', to: 'sessions#logging_in' 
+  get 'users/signup', to: 'sessions#new', as: :signup #signup_path
+  post 'signup', to: 'sessions#create' 
+  get 'users/login', to: 'sessions#login', as: :login #login_path
+  post 'login', to: 'sessions#logging_in' 
+  get 'logout', to: 'sessions#logout' #logout_path
 
   resources :decks #index, new, create, edit, update, destroy
 end
