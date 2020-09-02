d49eccc4b8b495f39e4a52b05e10294f69988768
Allan5829 <allan.farca@gmail.com>
2020-09-01 21:32:15 -0700

    creates sessions controller and creates custom routes for sessions and basic routes for decks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #For sessions
+  get '/users/signup', to: 'sessions#new', as: :signup 
+  post '/signup', to: 'sessions#create' 
+  get '/users/login', to: 'sessions#login', as: :login 
+  post '/login', to: 'sessions#logging_in' 
+
+  resources :decks #index, new, create, edit, update, destroy
 end
