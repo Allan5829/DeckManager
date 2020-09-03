5ce565afb5ec43d9c7893bc09233d59f9d43e164
Allan5829 <allan.farca@gmail.com>
2020-09-03 03:51:22 -0700

    creates functionality for the decks index action.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,9 +1,11 @@
 class DecksController < ApplicationController 
 
     def index
+        @decks = Deck.where(shared: true)
     end 
 
     def show
+        @deck = Deck.find_by(id: params[:id])
     end 
 
     def new
