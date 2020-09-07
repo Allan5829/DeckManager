1bd536dc666abbded03f6d7cb6462078c88e3500
Allan5829 <allan.farca@gmail.com>
2020-09-07 06:32:50 -0700

    adds validations and/or reorganizes Deck and Card models.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,18 +1,18 @@
 class Card < ApplicationRecord
     belongs_to :deck
 
-    #validate :duplicate_name
+    # Card Object Validations
+    
+    validates :name, presence: true
     validates :basic_energy, inclusion: { in: [true, false] }
-
     validates :count, inclusion: { in: [1, 2, 3, 4], 
         message: ", you can only play with 1-4 copies of the same card in a deck, unless it's basic energy" },
         if: :not_basic_energy?
 
+    # Validation Methods Start
+
     def not_basic_energy?
         !self.basic_energy
-    end 
-
-    def duplicate
     end 
 
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,36 +3,21 @@ class Deck < ApplicationRecord
     has_many :users, through: :user_decks
     belongs_to :tournament
     has_many :cards
+
     accepts_nested_attributes_for :cards
 
-    #removes cards that have no name so cards aren't created with a blank name
+    # removes cards that have no name and count so blank cards aren't saved
     before_validation :remove_empty_cards 
 
-    #deck object validations
+    # Deck Object Validations
     validates :title, presence: true
     validates :shared, inclusion: { in: [true, false] }
+    validate :deck_count # deck must have 60 cards
+    validate :no_duplicate # can't create 2 cards with the same name
 
-    #card object validations
-    validate :deck_count
+    #Model Methods Start
 
-    def deck_count
-        deck_count = 0
-        self.cards.each do |card|
-            deck_count += card.count
-        end 
-        if deck_count < 60
-            errors.add(:deck, "has less then 60 cards")
-        elsif deck_count > 60
-            errors.add(:deck, "has more then 60 cards")
-        end 
-    end
-
-
-
-
-
-
-    @rows = 2
+    @rows = 20
 
     def self.row_increase
         @rows += 1
@@ -46,21 +31,46 @@ class Deck < ApplicationRecord
         @rows
     end
 
-    
-    
+    # Validation Methods Start
 
+    def deck_count
+        deck_count = 0
+
+        self.cards.each do |card|
+            deck_count += card.count
+        end 
+
+        if deck_count < 60
+            errors.add(:deck, "has less then 60 cards")
+        elsif deck_count > 60
+            errors.add(:deck, "has more then 60 cards")
+        end 
+    end
+
+    def no_duplicate
+        card_array = []
+
+        self.cards.each do |card|
+            card_array << card.name
+        end
+
+        if card_array.uniq.length != card_array.length
+            errors.add(:deck, "can't have the same card in different rows")
+        end 
+    end 
 
     private
 
     def remove_empty_cards
         blank_card = []
+
         self.cards.each do |card|
             if !card.name.present? && !card.count.present?
                 blank_card << card 
             end 
         end 
-        self.cards = cards - blank_card
 
+        self.cards = cards - blank_card
         true
     end
 
